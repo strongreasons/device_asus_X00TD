12d4bfb421db5a15dba030c7b741d9198b24c380
Sushil Chauhan <sushilchauhan@codeaurora.org>
2017-02-28 12:42:59 -0800

    X00TD: disable partial split strategy in sdm
    Disable the partial SRC split strategy in SDM.
    
    Change-Id: Idbeebfc1ff2374f7c9a8101a69c84c4f5aa978af
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,6 +108,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     ro.opengles.version=196610 \
+    vendor.display.disable_partial_split=1 \
     sdm.debug.rotator_downscale=1 \
     debug.sf.enable_gl_backpressure=1 \
     vendor.display.enable_default_color_mode=1
