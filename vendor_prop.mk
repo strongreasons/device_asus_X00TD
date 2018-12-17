c06705290f268160c7b1f01e7cdd14d428bd9713
Artem Borisov <dedsa2002@gmail.com>
2018-12-17 18:02:34 +0400

    X00TD: Disable debug.sf.latch_unsignaled
    Apparently, it introduces dramatical performance degradation in some use cases.
    
    Change-Id: I92aaa4469f6848e4dfff89f3781f31ed527bae2b
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,7 +103,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
