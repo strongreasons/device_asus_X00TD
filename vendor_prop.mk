6160f7496b12cbda24ac04ac5fcde417166bc807
Ady Abraham <adyabr@google.com>
2019-07-22 13:11:45 -0700

    X00T: prop: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -111,6 +111,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.sf.lcd_density=480 \
     sdm.debug.rotator_downscale=1 \
+    debug.sf.enable_gl_backpressure=1 \
     vendor.display.enable_default_color_mode=1
 
 # IMS
