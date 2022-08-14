43af323b4089f7ac319cfc4f877a66a84031e7c3
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-15 01:23:01 +0700

    X00TD: Proton lunch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_X00TD.mk
+    $(LOCAL_DIR)/proton_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_X00TD-userdebug \
-    komodo_X00TD-user \
-    komodo_X00TD-eng
+    proton_X00TD-userdebug \
+    proton_X00TD-user \
+    proton_X00TD-eng
