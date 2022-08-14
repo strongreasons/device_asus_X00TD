13279f2ac5a7a3cdcfd36840586291df2a3dfac0
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-15 01:24:13 +0700

    X00TD: Proton bringup

--- komodo_X00TD.mk
+++ komodo_X00TD.mk
@@ -10,11 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common KomodoOS stuff
-$(call inherit-product, vendor/komodo/config/common.mk)
+$(call inherit-product, vendor/proton/build/product.mk)
 
-# Komodo Stuff
-KOMODO_GAPPS_TYPE := gapps
-KOMODO_VARIANT := RELEASE
+# Proton Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
@@ -22,7 +20,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := komodo_X00TD
+PRODUCT_NAME := proton_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
