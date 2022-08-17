1edcd955851b6f637f342381ac63034f71e4dca2
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2022-08-17 02:20:02 +0000

    X00TD: Bump to raven r22 fingerprint

--- aicp_X00TD.mk
+++ aicp_X00TD.mk
@@ -30,7 +30,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8672226 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP3A.220705.004 8836240 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.004/8836240:user/release-keys"
