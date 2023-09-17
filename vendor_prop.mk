0553de432879e1beaf038a88299c40602a90b71e
strongreasons <abenkenari@gmail.com>
2023-09-17 15:27:32 +0000

    X00TD: Fix screen recorder lag
    Change-Id: I9fc34d6faf4b56e3ac0beb5e3517728b7faac5da

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,6 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
+    debug.sdm.support_writeback \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
