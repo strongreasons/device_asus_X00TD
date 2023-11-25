7ac23ff6a3726d06b3b03412560c8683b2c5c2b8
strongreasons <abenkenari@gmail.com>
2023-11-25 15:31:10 +0000

    X00T: Havoc Bringup!
    Change-Id: I23d46ec217be087bacc6d4fdae800a94a4077bc6

--- havoc_X00T.mk
+++ havoc_X00T.mk
@@ -9,20 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HavocOS stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := false
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00T
+PRODUCT_NAME := havoc_X00T
 PRODUCT_DEVICE := X00T
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
