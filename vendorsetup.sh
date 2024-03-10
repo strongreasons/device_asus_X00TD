b2367b1b0212e08de5df5c55b6d29a7adbe82ead
strongreasons <109900777+strongreasons@users.noreply.github.com>
2024-03-11 01:24:36 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b tom/eas kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/display
