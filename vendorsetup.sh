34321c798234e62ad255ac7f10e64624806c8e7e
strongreasons <109900777+strongreasons@users.noreply.github.com>
2024-03-11 20:35:42 +0700

    X00TD: track fwb from our fork

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/strongreasons/pe_frameworks_base -b thirteen-plus frameworks/base
 rm -rf kernel/asus/sdm660
 git clone --recursive --depth=1 https://github.com/RyuujiX/android_kernel_asus_sdm660 -b eas-test kernel/asus/sdm660 && rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf hardware/qcom-caf/msm8998/audio
