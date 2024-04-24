22f2e7c6e452873fb7d44bb39b4d5dc1f71a579f
strongreasons <109900777+strongreasons@users.noreply.github.com>
2024-04-24 07:54:29 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug \
-    lineage_X00TD-user \
-    lineage_X00TD-eng
+    lineage_X00TD-ap1a-userdebug \
+    lineage_X00TD-ap1a-user \
+    lineage_X00TD-ap1a-eng
