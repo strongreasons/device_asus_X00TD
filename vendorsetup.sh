ea0566dda87a255883b0bd13ef5198f5f8b3c134
strongreasons <109900777+strongreasons@users.noreply.github.com>
2024-04-26 12:21:37 +0700

    X00TD: Switch to crdroid jem
    but

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,7 @@
 rm -rf kernel/asus/sdm660
 git clone --recursive --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
 
+rm -rf external/jemalloc_new
+git clone --depth=1 https://github.com/crdroidandroid/android_external_jemalloc-new -b 14.0 external/jemalloc_new
+
 export TZ=Asia/Jakarta
