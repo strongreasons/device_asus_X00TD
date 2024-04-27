6577f0ece27b38369418bb788c6754749ba8bb12
strongreasons <109900777+strongreasons@users.noreply.github.com>
2024-04-27 20:25:45 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 rm -rf kernel/asus/sdm660
 git clone --recursive --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
 
-rm -rf external/jemalloc_new
-git clone --depth=1 https://github.com/crdroidandroid/android_external_jemalloc-new -b 14.0 external/jemalloc_new
+rm -rf device/asus/sdm660-common
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b 14.0 device/asus/sdm660-common
 
 export TZ=Asia/Jakarta
