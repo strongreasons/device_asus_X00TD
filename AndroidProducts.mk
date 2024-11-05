7e6094a76782ec8e1953ed03c09deb70c09d2c25
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2024-11-06 04:07:14 +0700

    X00TD: Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_X00TD.mk
+    $(LOCAL_DIR)/superior_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_X00TD-eng \
-    afterlife_X00TD-user \
-    afterlife_X00TD-userdebug
+    superior_X00TD-ap1a-eng \
+    superior_X00TD-ap1a-user \
+    superior_X00TD-ap1a-userdebug
