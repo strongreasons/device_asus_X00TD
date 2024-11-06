ed6fc6f3150a4f162002b3da2f5dc3a00777c8b4
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2024-11-07 04:15:49 +0700

    X00T: Fix libfmjni

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,6 +16,14 @@ git clone --depth=1 https://github.com/strongreasons/vendor_asus -b main vendor/
 rm -rf vendor/lineage-priv/keys
 git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys
 
+cd packages/apps/FMRadio
+git revert 3a33db46414421a81e75cc4af07bc9b4eba528f2
+cd ../../..
+cd vendor/qcom/opensource/libfmjni
+git revert 055b0bed7a5f8822fc7c4658fff44c886177cb9b
+cd ../../../..
+
 sed -i 's/19/21/g' prebuilts/sdk/current/androidx/m2repository/androidx/preference/preference/1.3.0-alpha01/manifest/AndroidManifest.xml
+
 export KBUILD_BUILD_USER=queen
 export TZ=Asia/Jakarta
