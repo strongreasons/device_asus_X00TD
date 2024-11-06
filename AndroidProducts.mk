f016eb17e73cf8c82c9f69b272bd1bbc55452404
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2024-11-06 21:33:33 +0700

    X00TD: Switch to ap2a aosp tag

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/superior_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_X00TD-ap1a-eng \
-    superior_X00TD-ap1a-user \
-    superior_X00TD-ap1a-userdebug
+    superior_X00TD-ap2a-eng \
+    superior_X00TD-ap2a-user \
+    superior_X00TD-ap2a-userdebug
