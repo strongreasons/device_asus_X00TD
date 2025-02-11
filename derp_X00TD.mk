46fc841b21ed89ad0fc917404a8e3737ea46e6cd
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-12 00:22:18 +0700

    X00TD: Add build fingerprint

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -28,4 +28,8 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD
+    DeviceProduct=ASUS_X00TD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
