d732ec29c03a04e30d1ed9ffc77b4e7657020609
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-11 17:57:46 +0700

    X00TD: Create derp_X00TD.mk

---
+++ derp_X00TD.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common DerpFest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+
+# Gapps
+TARGET_USES_MINI_GAPPS := true
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := derp_X00TD
+PRODUCT_DEVICE := X00TD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=ASUS_X00TD
