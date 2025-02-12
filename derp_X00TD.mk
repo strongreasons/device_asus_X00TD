427bcba78b92d0c2d702545b119fe7ef58edcc3a
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-12 09:14:12 +0700

    X00TD: Drop piv desc

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -28,8 +28,6 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+    DeviceProduct=ASUS_X00TD
 
-# Build fingerprint
 BUILD_FINGERPRINT := "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
