ba3c23e71e6d94958d1ecd9cc3eba0d4facbdcd8
strongreasons <abenkenari@gmail.com>
2025-02-15 15:32:48 +0000

    X00TD: Update vendorsetup
    Change-Id: If29c445c4b375d82f3d9bbebe9bc57d7cd7b2f00

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,12 @@ rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
+rm -rf device/asus/sdm660-common
+git clone https://github.com/strongreasons/device_asus_sdm660-common -b 15 device/asus/sdm660-common
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/strongreasons/vendor_asus -b 15 vendor/asus
+
 rm -rf vendor/lineage-priv/keys
 mkdir -p vendor/lineage-priv/keys
 git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys/
