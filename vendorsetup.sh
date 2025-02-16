6c270771118a77cc87f17d7c9fa11f758a61e0f0
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-17 02:52:08 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,10 +3,10 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus/sdm660-common
-git clone https://github.com/strongreasons/device_asus_sdm660-common -b 15 device/asus/sdm660-common
+git clone https://github.com/SonicBSV/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
 
 rm -rf vendor/asus
-git clone --depth=1 https://github.com/strongreasons/vendor_asus -b 15 vendor/asus
+git clone --depth=1 https://github.com/SonicBSV/proprietary_vendor_asus-4.19 vendor/asus
 
 rm -rf vendor/lineage-priv/keys
 mkdir -p vendor/lineage-priv/keys
