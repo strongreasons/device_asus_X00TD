b03d40db43cb662aa27f53ba885c41feaa0b3a41
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-18 21:37:57 +0700

    X00TD: Change kerneltree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
-rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
+git clone --depth=1 https://github.com/SonicBSV/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
+#rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus/sdm660-common
 git clone https://github.com/SonicBSV/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
