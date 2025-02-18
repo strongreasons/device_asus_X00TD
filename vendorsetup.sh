fa300075a152c9651eda560a5886db6d688beb3b
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-02-19 03:40:57 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,8 @@ git clone https://github.com/strongreasons/device_asus_sdm660-common -b 15 devic
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/strongreasons/vendor_asus -b 15 vendor/asus
 
-rm -rf vendor/lineage-priv/keys
-mkdir -p vendor/lineage-priv/keys
-git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys/
+#rm -rf vendor/lineage-priv/keys
+#mkdir -p vendor/lineage-priv/keys
+#git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys/
 
 export TZ=Asia/Jakarta
