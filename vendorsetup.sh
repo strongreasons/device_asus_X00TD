c20a2166c3df2ebbc6173d82750fd1c320f1bd3b
strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>
2025-05-17 09:19:17 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,19 +3,27 @@
 export BUILD_USER=queen
 export TZ=Asia/Jakarta
 
+# Clone kernel tree
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b stable kernel/asus/sdm660
+git clone --depth=1 https://github.com/Teamhackneyed/android_kernel_asus_sdm660 -b lineage-22.1 kernel/asus/sdm660
 
 # Force Using KSU
-rm -rf system/extras/su
-rm -rf platform/system/extras/su
+#rm -rf system/extras/su
+#rm -rf platform/system/extras/su
 
+# Clone common tree
 rm -rf device/asus/sdm660-common
 git clone --depth=1 https://github.com/strongreasons/device_asus_sdm660-common -b v2 device/asus/sdm660-common
 
+# Clone common tree
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/proprietary_vendor_asus-4.19 -b 15.0 vendor/asus
 
+# Clone qcom vibrator
+rm -rf vendor/qcom/opensource/vibrator
+git clone --depth=1 https://github.com/electrolaboratory/android_vendor_qcom_opensource_vibrator -b lineage-22.2 vendor/qcom/opensource/vibrator
+
+# Clone keys
 rm -rf vendor/lineage-priv/keys
 rm -rf vendor/lineage/signing/keys
 mkdir -p vendor/lineage-priv/keys
