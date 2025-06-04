59b565658d3f95f35a0334ccf5436e1e3d3f0fa5
SonicBSV <sonicbsv@ya.ru>
2025-06-04 18:27:48 +0300

    X00TD: drop thermal-engine config

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
-# Thermal configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Wifi
 PRODUCT_PACKAGES += \
     X00TDWifiOverlay
